788e1cd9825a990b2ad3b2a49dba13691cb72a5f
HBalu1026 <71315012+HBalu1026@users.noreply.github.com>
2023-04-02 22:30:50 +0200

    Add files via upload

---
+++ Vadasz_Denes_verseny-main/vadaszdenes/index.js
@@ -0,0 +1,25 @@
+var elozoIndex = 0;
+function aktiv(index){
+    var elemek = document.getElementsByClassName("navElemek");
+    elemek[elozoIndex].classList.remove("active");
+    elemek[index].classList.add("active");
+    elozoIndex = index;
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
